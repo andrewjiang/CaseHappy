161ecbc352919178ef66c4ad7c752f8e02a2b02e
Andrew Jiang <andrew.h.jiang@gmail.com>
2014-04-28 22:43:16 -0500

    Add back two lines in local environment rb

--- config/initializers/local_environment.rb
+++ config/initializers/local_environment.rb
@@ -1,5 +1,7 @@
 # Set local environment variables... securely?
-env = YAML.load_file(Rails.root.join('config/environment.yml'))[Rails.env]
-env.each do |key, value|
-  ENV[key.upcase] = value
+unless Rails.env.production?
+	env = YAML.load_file(Rails.root.join('config/environment.yml'))[Rails.env]
+	env.each do |key, value|
+	  ENV[key.upcase] = value
+	end
 end
